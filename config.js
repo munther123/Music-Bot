e091ced33883bbfceb1fdd6c2bec553699d9837f
munther123 <56301140+munther123@users.noreply.github.com>
2019-10-08 13:51:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NjMxMDcxNjU4ODc0MTA5OTY0.XZxmMQ.d4b8e82QA0l7UrTJo9mb7FM_ZyA';
 
 exports.PREFIX = 'm!';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyABS2TqYQds509VKN0A2TiZrPPQBaKrs34';
